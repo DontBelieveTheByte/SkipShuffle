f16ecc650b0db49ba61bf39fb46f1d8f1050e316
killthesystem <ever.livin.never.fearful@gmail.com>
2014-07-08 13:46:29 -0400

    Define media player commands.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/SkipShuflleMediaPlayerCommands.java
@@ -0,0 +1,10 @@
+package com.dontbelievethebyte.skipshuffle;
+
+public class SkipShuflleMediaPlayerCommands {
+    public static final int CMD_PLAY = 1;
+    public static final int CMD_PAUSE = 2;
+    public static final int CMD_SKIP = 3;
+    public static final int CMD_PREV = 4;
+    public static final int CMD_SHUFFLE_PLAYLIST = 5;
+    public static final int CMD_GET_PLAYER_STATE = 6;
+}
