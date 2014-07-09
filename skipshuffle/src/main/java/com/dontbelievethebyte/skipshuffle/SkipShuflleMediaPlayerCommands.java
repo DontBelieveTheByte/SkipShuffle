d9a368bbe4e7a86b0795ce89d0ee3b637377c4cb
killthesystem <ever.livin.never.fearful@gmail.com>
2014-07-09 15:01:22 -0400

    General command string.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/SkipShuflleMediaPlayerCommands.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/SkipShuflleMediaPlayerCommands.java
@@ -1,10 +1,11 @@
 package com.dontbelievethebyte.skipshuffle;
 
 public class SkipShuflleMediaPlayerCommands {
-    public static final int CMD_PLAY = 1;
-    public static final int CMD_PAUSE = 2;
-    public static final int CMD_SKIP = 3;
-    public static final int CMD_PREV = 4;
-    public static final int CMD_SHUFFLE_PLAYLIST = 5;
-    public static final int CMD_GET_PLAYER_STATE = 6;
+    public static final String COMMAND = "com.dontbelievethebyte.cmd.COMMAND";
+    public static final String CMD_PLAY = "com.dontbelievethebyte.cmd.PLAY";
+    public static final String CMD_PAUSE = "com.dontbelievethebyte.cmd.PAUSE";
+    public static final String CMD_SKIP = "com.dontbelievethebyte.cmd.SKIP";
+    public static final String CMD_PREV = "com.dontbelievethebyte.cmd.PREV";
+    public static final String CMD_SHUFFLE_PLAYLIST = "com.dontbelievethebyte.cmd.SHUFFLE_PLAYLIST";
+    public static final String CMD_GET_PLAYER_STATE = "com.dontbelievethebyte.cmd.GET_PLAYER_STATE";
 }
