1c3bcbd8e7fdb58879ca3ccb85daf340ff467cfc
killthesystem <ever.livin.never.fearful@gmail.com>
2014-08-04 00:33:41 -0400

    Augmented class to return player state + fixed types.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/MediaPlayerBroadcastReceiver.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/MediaPlayerBroadcastReceiver.java
@@ -9,6 +9,7 @@ public class MediaPlayerBroadcastReceiver extends BroadcastReceiver{
     private static final String TAG = "PLAYER_BROADCAST_RECEIVER";
     private long playlistID;
     private int playlistPosition;
+    private String playerState;
 
     public long getPlaylistID() {
         return playlistID;
@@ -16,16 +17,18 @@ public class MediaPlayerBroadcastReceiver extends BroadcastReceiver{
     public int getPlaylistPosition(){
         return playlistPosition;
     }
+    public String getPlayerState() {return playerState;}
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.PLAYLIST_ID)){
-            playlistID = intent.getLongExtra(SkipShuflleMediaPlayerCommandsContract.PLAYLIST_ID, 0);
-            Log.v(TAG, Long.toString(playlistID));
+            playlistID = intent.getLongExtra(SkipShuflleMediaPlayerCommandsContract.PLAYLIST_ID, 1);
         }
         if(intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.PLAYLIST_CURSOR_POSITION)){
             playlistID = intent.getLongExtra(SkipShuflleMediaPlayerCommandsContract.PLAYLIST_ID, 0);
-            Log.v(TAG, Integer.toString(playlistPosition));
+        }
+        if(intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.CMD_GET_PLAYER_STATE)){
+            playerState = intent.getStringExtra(SkipShuflleMediaPlayerCommandsContract.CMD_GET_PLAYER_STATE);
         }
     }
 }
