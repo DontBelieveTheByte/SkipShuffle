0112a9636aa2944096c1eca5b8a047d3ab29eaa6
killthesystem <ever.livin.never.fearful@gmail.com>
2014-08-05 02:38:01 -0400

    Added track metadata getters and setters.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/Track.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/Track.java
@@ -1,24 +1,44 @@
 package com.dontbelievethebyte.skipshuffle;
 
 public class Track {
-    private long _id;
-    private String _path;
+    private long id;
+    private String path;
+    private String title;
+    private String artist;
+    private String album;
+    private String genre;
 
     public Track(){}
 
     public long getId() {
-        return _id;
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getPath() {
-        return _path;
+        return path;
     }
 
-    public void setPath(String _path) {
-        this._path = _path;
-    }
-    public void setId(long id) {
-        this._id = id;
+    public void setPath(String path) {
+        this.path = path;
     }
 
+    public String getTitle() { return title;}
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getArtist() { return artist; }
+
+    public void setArtist(String artist) { this.artist = artist; }
+
+    public String getAlbum() { return album; }
+
+    public void setAlbum(String album) { this.album = album; }
+
+    public String getGenre() { return genre; }
+
+    public void setGenre(String genre) { this.genre = genre; }
 }
