b5d7467ea209755ce74fa8b84b8e233cece0064f
killthesystem <ever.livin.never.fearful@gmail.com>
2014-08-19 03:18:17 -0400

    Changed UI to interface.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/UI.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/UI.java
@@ -1,59 +1,22 @@
 package com.dontbelievethebyte.skipshuffle;
 
 import android.graphics.Typeface;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-public class UI {
-    public static final int MONO_LIGHT = 0;
-    public static final int MONO_DARK = 1;
-    public static final int NEON = 3;
-    public static final int MARIO = 4;
+public interface UI {
 
-    public static UI createUI(MainActivity mainActivity, Integer uiType){
-        switch (uiType){
-            case 0 :
-                return new MonoLightUI(mainActivity);
-            case 1 :
-                return new MonoDarkUI(mainActivity);
-            case 2 :
-                return new NeonUI(mainActivity);
-            case 4 :
-                return new MarioUI(mainActivity);
-            default:
-                return new NeonUI(mainActivity);
-        }
-    }
+    public void doPlay();
 
-    protected MainActivity mainActivity;
+    public void doPause();
 
-    public ImageButton playlistBtn;
-    public ImageButton prevBtn;
-    public ImageButton playBtn;
-    public ImageButton shuffleBtn;
-    public ImageButton skipBtn;
-    protected TextView songTitle;
-    protected Typeface typeface;
+    public void doSkip();
 
-    public UI(MainActivity mainActivity){
-        this.mainActivity = mainActivity;
-    }
+    public void doPrev();
 
-    public void doPlay() {}
+    public void doShuffle();
 
-    public void doPause() {}
+    public void reboot();
 
-    public void doSkip() {}
+    public void setSongTitle();
 
-    public void doPrev() {}
-
-    public void doShuffle() {}
-
-    public void reboot(){}
-
-    public void setSongTitle(String title){}
-
-    public Typeface getTypeFace(){
-        return typeface;
-    }
+    public Typeface getTypeFace();
 }
