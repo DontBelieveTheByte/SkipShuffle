18fa7be78435774fe7dbf0b76732e175cc618643
killthesystem <ever.livin.never.fearful@gmail.com>
2014-08-19 02:45:43 -0400

    Updated UI factory to switch type.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/UI.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/UI.java
@@ -5,12 +5,25 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class UI {
+    public static final int MONO_LIGHT = 0;
+    public static final int MONO_DARK = 1;
+    public static final int NEON = 3;
+    public static final int MARIO = 4;
 
-    public static UI createUI(MainActivity mainActivity){
-        return new NeonUI(mainActivity);
+    public static UI createUI(MainActivity mainActivity, Integer uiType){
+        switch (uiType){
+            case 0 :
+                return new MonoLightUI(mainActivity);
+            case 1 :
+                return new MonoDarkUI(mainActivity);
+            case 2 :
+                return new NeonUI(mainActivity);
+            case 4 :
+                return new MarioUI(mainActivity);
+            default:
+                return new NeonUI(mainActivity);
+        }
     }
-
-    private static final String TAG = "SkipShuffleUI";
 
     protected MainActivity mainActivity;
 
