6a8120dbd4905a4afba1c2dc613f561fe9399642
killthesystem <ever.livin.never.fearful@gmail.com>
2014-08-28 19:15:33 -0400

    Specified mono dark/light uis for playlist activity.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/MonoDarkPlaylistUI.java
@@ -0,0 +1,56 @@
+package com.dontbelievethebyte.skipshuffle;
+
+import android.graphics.Typeface;
+import android.widget.ImageButton;
+
+public class MonoDarkPlaylistUI extends PlaylistUI {
+
+    public MonoDarkPlaylistUI(PlaylistActivity playlistActivity) {
+        super(playlistActivity);
+        playlistActivity.setContentView(R.layout.mono_dark_activity_playlist);
+        playlistPlayBtn = (ImageButton) playlistActivity.findViewById(R.id.playlist_layout_play);
+        playlistShuffleBtn = (ImageButton) playlistActivity.findViewById(R.id.playlist_layout_shuffle);
+        playlistSkipBtn = (ImageButton) playlistActivity.findViewById(R.id.playlist_layout_skip);
+        playlistPrevBtn = (ImageButton) playlistActivity.findViewById(R.id.playlist_layout_prev);
+    }
+
+    @Override
+    public void doPlay() {
+
+    }
+
+    @Override
+    public void doPause() {
+
+    }
+
+    @Override
+    public void doSkip() {
+
+    }
+
+    @Override
+    public void doPrev() {
+
+    }
+
+    @Override
+    public void doShuffle() {
+
+    }
+
+    @Override
+    public void reboot() {
+
+    }
+
+    @Override
+    public void setSongTitle(String title) {
+
+    }
+
+    @Override
+    public Typeface getTypeFace() {
+        return null;
+    }
+}
