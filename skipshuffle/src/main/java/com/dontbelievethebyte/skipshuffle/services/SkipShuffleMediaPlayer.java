34ac9f0c8d36247eae4a86dacfd35e2850b5545b
killthesystem <ever.livin.never.fearful@gmail.com>
2014-09-02 16:45:51 -0400

    Reordered methods.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/SkipShuffleMediaPlayer.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/SkipShuffleMediaPlayer.java
@@ -60,13 +60,6 @@ public class SkipShuffleMediaPlayer extends Service implements PreferenceChanged
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        //return super.onStartCommand(intent, flags, startId);
-        return START_STICKY;
-    }
-
-    @Override
     public IBinder onBind(Intent intent)
     {
         return null;
@@ -117,6 +110,37 @@ public class SkipShuffleMediaPlayer extends Service implements PreferenceChanged
         preferencesHelper.setLastPlaylistPosition(playlist.getPosition());
     }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        //return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void preferenceChangedCallback(String prefsKey)
+    {
+        if (getString(R.string.pref_current_playlist_id).equals(prefsKey)) {
+            try {
+                playlist = new RandomPlaylist(
+                        preferencesHelper.getLastPlaylist(),
+                        dbHandler
+                );
+                playlist.setPosition(0);
+                broadcastCurrentState();
+                playerWrapper.setPlaylist(playlist);
+            } catch (JSONException jsonException){
+                Toast.makeText(
+                        getApplicationContext(),
+                        String.format(getString(R.string.playlist_load_error), preferencesHelper.getLastPlaylist()),
+                        Toast.LENGTH_LONG
+                ).show();
+                preferencesHelper.setLastPlaylist(1);
+                preferencesHelper.setLastPlaylistPosition(0);
+            }
+        }
+    }
+
     public void registerMediaPlayerBroadcastReceiver()
     {
         if (null == clientCommandsBroadcastReceiver) {
@@ -129,11 +153,100 @@ public class SkipShuffleMediaPlayer extends Service implements PreferenceChanged
         intentFilter.addAction(Intent.ACTION_MEDIA_BUTTON);
         registerReceiver(clientCommandsBroadcastReceiver, intentFilter);
     }
+
     public void unregisterMediaPlayerBroadcastReceiver()
     {
         if(clientCommandsBroadcastReceiver != null){
             unregisterReceiver(clientCommandsBroadcastReceiver);
         }
+    }
+
+    private void broadcastCurrentState()
+    {
+        Intent intent = new Intent(SkipShuflleMediaPlayerCommandsContract.CURRENT_STATE);
+        String formattedTitle;
+        try {
+            formattedTitle = buildFormattedTitle(playlist.getCurrent());
+        } catch (PlaylistEmptyException e){
+            formattedTitle = buildFormattedTitle(null);
+        }
+        intent.putExtra(
+                            SkipShuflleMediaPlayerCommandsContract.CURRENT_STATE,
+                            playerWrapper.isPlaying() ?
+                                    SkipShuflleMediaPlayerCommandsContract.STATE_PLAY :
+                                    SkipShuflleMediaPlayerCommandsContract.STATE_PAUSE
+                        )
+              .putExtra(
+                            SkipShuflleMediaPlayerCommandsContract.STATE_PLAYLIST_ID,
+                            playlist.getId()
+                        )
+              .putExtra(
+                            SkipShuflleMediaPlayerCommandsContract.STATE_CURRENT_SONG_TITLE,
+                            formattedTitle
+                       )
+              .putExtra(
+                      SkipShuflleMediaPlayerCommandsContract.STATE_PLAYLIST_POSITION,
+                      playlist.getPosition()
+              );
+        sendStickyBroadcast(intent);
+    }
+
+    private String buildFormattedTitle(Track track)
+    {
+        if (null == track) {
+            return getApplicationContext().getString(R.string.meta_data_unknown_current_song_title);
+        }
+        else if (null == track.getArtist() || null == track.getTitle()) {
+            return (null == track.getPath()) ?
+                    getApplicationContext().getString(R.string.meta_data_unknown_current_song_title) :
+                    track.getPath().substring(track.getPath().lastIndexOf("/") + 1);
+        } else {
+            return track.getArtist() + " - " + track.getTitle();
+        }
+    }
+
+    private PendingIntent buildNotificationButtonsPendingIntent(String command, int requestCode)
+    {
+        Intent intent = new Intent(SkipShuflleMediaPlayerCommandsContract.COMMAND);
+        intent.putExtra(SkipShuflleMediaPlayerCommandsContract.COMMAND, command);
+        return PendingIntent.getBroadcast(
+                this,
+                requestCode,
+                intent,
+                PendingIntent.FLAG_CANCEL_CURRENT
+        );
+    }
+
+    private void handleCommand(String command, Intent intent)
+    {
+        if (SkipShuflleMediaPlayerCommandsContract.CMD_PLAY_PAUSE_TOGGLE.equals(command)){
+            if (intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.CMD_SET_PLAYLIST_CURSOR_POSITION)) {
+                playlist.setPosition(
+                        intent.getIntExtra(SkipShuflleMediaPlayerCommandsContract.CMD_SET_PLAYLIST_CURSOR_POSITION,
+                                0
+                        )
+                );
+                playerWrapper.doPlay();
+            } else {
+                if (playerWrapper.isPlaying()) {
+                    playerWrapper.doPause();
+                } else {
+                    playerWrapper.doPlay();
+                }
+            }
+        } else if (SkipShuflleMediaPlayerCommandsContract.CMD_SKIP.equals(command)) {
+            playerWrapper.doSkip();
+        } else if (SkipShuflleMediaPlayerCommandsContract.CMD_PREV.equals(command)) {
+            playerWrapper.doPrev();
+        } else if (SkipShuflleMediaPlayerCommandsContract.CMD_SHUFFLE_PLAYLIST.equals(command)){
+            playerWrapper.doShuffle();
+        }
+    }
+
+    private void removeNotification()
+    {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
     }
 
     private void setNotification()
@@ -194,124 +307,12 @@ public class SkipShuffleMediaPlayer extends Service implements PreferenceChanged
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
         notificationBuilder.setSmallIcon(R.drawable.ic_notification)
-                           .setContent(remoteViews);
+                .setContent(remoteViews);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(
                 NOTIFICATION_ID,
                 notificationBuilder.build()
         );
-    }
-
-    private void removeNotification()
-    {
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.cancel(NOTIFICATION_ID);
-    }
-
-    private PendingIntent buildNotificationButtonsPendingIntent(String command, int requestCode)
-    {
-        Intent intent = new Intent(SkipShuflleMediaPlayerCommandsContract.COMMAND);
-        intent.putExtra(SkipShuflleMediaPlayerCommandsContract.COMMAND, command);
-        return PendingIntent.getBroadcast(
-                this,
-                requestCode,
-                intent,
-                PendingIntent.FLAG_CANCEL_CURRENT
-        );
-    }
-
-    private void broadcastCurrentState()
-    {
-        Intent intent = new Intent(SkipShuflleMediaPlayerCommandsContract.CURRENT_STATE);
-        String formattedTitle;
-        try {
-            formattedTitle = buildFormattedTitle(playlist.getCurrent());
-        } catch (PlaylistEmptyException e){
-            formattedTitle = buildFormattedTitle(null);
-        }
-        intent.putExtra(
-                            SkipShuflleMediaPlayerCommandsContract.CURRENT_STATE,
-                            playerWrapper.isPlaying() ?
-                                    SkipShuflleMediaPlayerCommandsContract.STATE_PLAY :
-                                    SkipShuflleMediaPlayerCommandsContract.STATE_PAUSE
-                        )
-              .putExtra(
-                            SkipShuflleMediaPlayerCommandsContract.STATE_PLAYLIST_ID,
-                            playlist.getId()
-                        )
-              .putExtra(
-                            SkipShuflleMediaPlayerCommandsContract.STATE_CURRENT_SONG_TITLE,
-                            formattedTitle
-                       )
-              .putExtra(
-                      SkipShuflleMediaPlayerCommandsContract.STATE_PLAYLIST_POSITION,
-                      playlist.getPosition()
-              );
-        sendStickyBroadcast(intent);
-    }
-
-    @Override
-    public void preferenceChangedCallback(String prefsKey)
-    {
-        if (getString(R.string.pref_current_playlist_id).equals(prefsKey)) {
-            try {
-                playlist = new RandomPlaylist(
-                        preferencesHelper.getLastPlaylist(),
-                        dbHandler
-                );
-                playlist.setPosition(0);
-                broadcastCurrentState();
-                playerWrapper.setPlaylist(playlist);
-            } catch (JSONException jsonException){
-                Toast.makeText(
-                    getApplicationContext(),
-                    String.format(getString(R.string.playlist_load_error), preferencesHelper.getLastPlaylist()),
-                        Toast.LENGTH_LONG
-                ).show();
-                preferencesHelper.setLastPlaylist(1);
-                preferencesHelper.setLastPlaylistPosition(0);
-            }
-        }
-    }
-
-    private String buildFormattedTitle(Track track)
-    {
-        if (null == track) {
-            return getApplicationContext().getString(R.string.meta_data_unknown_current_song_title);
-        }
-        else if (null == track.getArtist() || null == track.getTitle()) {
-            return (null == track.getPath()) ?
-                    getApplicationContext().getString(R.string.meta_data_unknown_current_song_title) :
-                    track.getPath().substring(track.getPath().lastIndexOf("/") + 1);
-        } else {
-            return track.getArtist() + " - " + track.getTitle();
-        }
-    }
-
-    private void handleCommand(String command, Intent intent)
-    {
-        if (SkipShuflleMediaPlayerCommandsContract.CMD_PLAY_PAUSE_TOGGLE.equals(command)){
-            if (intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.CMD_SET_PLAYLIST_CURSOR_POSITION)) {
-                playlist.setPosition(
-                        intent.getIntExtra(SkipShuflleMediaPlayerCommandsContract.CMD_SET_PLAYLIST_CURSOR_POSITION,
-                                0
-                        )
-                );
-                playerWrapper.doPlay();
-            } else {
-                if (playerWrapper.isPlaying()) {
-                    playerWrapper.doPause();
-                } else {
-                    playerWrapper.doPlay();
-                }
-            }
-        } else if (SkipShuflleMediaPlayerCommandsContract.CMD_SKIP.equals(command)) {
-            playerWrapper.doSkip();
-        } else if (SkipShuflleMediaPlayerCommandsContract.CMD_PREV.equals(command)) {
-            playerWrapper.doPrev();
-        } else if (SkipShuflleMediaPlayerCommandsContract.CMD_SHUFFLE_PLAYLIST.equals(command)){
-            playerWrapper.doShuffle();
-        }
     }
 }
