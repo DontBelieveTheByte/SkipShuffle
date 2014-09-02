a424f98b4b9639603a8e21342b35fbb29afc36dd
killthesystem <ever.livin.never.fearful@gmail.com>
2014-09-02 14:52:55 -0400

    Made class abstract + formatting.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/playlist/PlaylistUI.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/playlist/PlaylistUI.java
@@ -6,59 +6,26 @@ import android.widget.ImageButton;
 import com.dontbelievethebyte.skipshuffle.activities.PlaylistActivity;
 import com.dontbelievethebyte.skipshuffle.ui.UI;
 
-public class PlaylistUI implements UI {
-
-    protected PlaylistActivity playlistActivity;
+public abstract class PlaylistUI implements UI {
 
     public ImageButton playlistPlayBtn;
     public ImageButton playlistPrevBtn;
     public ImageButton playlistSkipBtn;
     public ImageButton playlistShuffleBtn;
 
-    public PlaylistUI(PlaylistActivity playlistActivity){
+    protected PlaylistActivity playlistActivity;
+
+    public PlaylistUI(PlaylistActivity playlistActivity)
+    {
         this.playlistActivity = playlistActivity;
-
     }
 
     @Override
-    public void doPlay() {
-
-    }
+    public void setSongTitle(String title) {}
 
     @Override
-    public void doPause() {
-
-    }
-
-    @Override
-    public void doSkip() {
-
-    }
-
-    @Override
-    public void doPrev() {
-
-    }
-
-    @Override
-    public void doShuffle() {
-
-    }
-
-    @Override
-    public void reboot() {
-
-    }
-
-    @Override
-    public void setSongTitle(String title) {
-
-    }
-
-    @Override
-    public Typeface getTypeFace() {
+    public Typeface getTypeFace()
+    {
         return null;
     }
-
-
 }
