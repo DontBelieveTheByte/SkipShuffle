682e525ca380824b77b1fe8d036d33bafb080cdc
killthesystem <ever.livin.never.fearful@gmail.com>
2014-09-02 14:06:38 -0400

    Formatting.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/playlist/Track.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/playlist/Track.java
@@ -10,35 +10,63 @@ public class Track {
 
     public Track(){}
 
-    public long getId() {
+    public long getId()
+    {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(long id)
+    {
         this.id = id;
     }
 
-    public String getPath() {
+    public String getPath()
+    {
         return path;
     }
 
-    public void setPath(String path) {
+    public void setPath(String path)
+    {
         this.path = path;
     }
 
-    public String getTitle() { return title;}
+    public String getTitle()
+    {
+        return title;
+    }
 
-    public void setTitle(String title) { this.title = title; }
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
 
-    public String getArtist() { return artist; }
+    public String getArtist()
+    {
+        return artist;
+    }
 
-    public void setArtist(String artist) { this.artist = artist; }
+    public void setArtist(String artist)
+    {
+        this.artist = artist;
+    }
 
-    public String getAlbum() { return album; }
+    public String getAlbum()
+    {
+        return album;
+    }
 
-    public void setAlbum(String album) { this.album = album; }
+    public void setAlbum(String album)
+    {
+        this.album = album;
+    }
 
-    public String getGenre() { return genre; }
+    public String getGenre()
+    {
+        return genre;
+    }
 
-    public void setGenre(String genre) { this.genre = genre; }
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
 }
