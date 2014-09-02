f783fc67687f165ded5d3f479d2618b4c377f5ee
killthesystem <ever.livin.never.fearful@gmail.com>
2014-09-02 14:12:48 -0400

    Formatting.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/MediaScannerBroadcastMessageContract.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/MediaScannerBroadcastMessageContract.java
@@ -4,5 +4,4 @@ public class MediaScannerBroadcastMessageContract {
     public static final String CURRENT_DIRECTORY_PROCESSING = "com.dontbelievethebyte.skipshuffle.currentDirectoryProcessing";
     public static final String CURRENT_FILE_PROCESSING = "com.dontbelievethebyte.skipshuffle.currentFileProcessing";
     public static final String IS_LAST_FILE_PROCESSING = "com.dontbelievethebyte.skipshuffle.isLastFileProcessing";
-
 }
