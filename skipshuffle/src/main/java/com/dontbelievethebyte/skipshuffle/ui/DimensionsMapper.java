75f8147312da4814209a635a7133d3403f859764
killthesystem <ever.livin.never.fearful@gmail.com>
2014-09-29 01:19:14 -0400

    Added dimensions mapper.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/DimensionsMapper.java
@@ -0,0 +1,6 @@
+package com.dontbelievethebyte.skipshuffle.ui;
+
+public final class DimensionsMapper {
+    public final static int drawerWidthDividerPortrait = 4;
+    public final static int drawerWidthDividerLandscape = 3;
+}
