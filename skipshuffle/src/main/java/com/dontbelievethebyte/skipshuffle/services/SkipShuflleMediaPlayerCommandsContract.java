5ef9dba552ab5c7a5a6508095ad180d79f938464
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-15 05:01:32 -0400

    Factored out state.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/SkipShuflleMediaPlayerCommandsContract.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/SkipShuflleMediaPlayerCommandsContract.java
@@ -6,13 +6,5 @@ public class SkipShuflleMediaPlayerCommandsContract {
     public static final String CMD_SKIP = "com.dontbelievethebyte.cmd.SKIP";
     public static final String CMD_PREV = "com.dontbelievethebyte.cmd.PREV";
     public static final String CMD_SHUFFLE_PLAYLIST = "com.dontbelievethebyte.cmd.SHUFFLE_PLAYLIST";
-    public static final String CMD_GET_PLAYER_STATE = "com.dontbelievethebyte.cmd.GET_PLAYER_STATE";
     public static final String CMD_SET_PLAYLIST_CURSOR_POSITION = "com.dontbelievethebyte.state.PLAYLIST_ID";
-    public static final String CURRENT_STATE = "com.dontbelievethebyte.state.STATE";
-    public static final String STATE_PLAY = "com.dontbelievethebyte.state.PLAY";
-    public static final String STATE_PAUSE = "com.dontbelievethebyte.state.PAUSE";
-    public static final String STATE_PLAYLIST_ID = "com.dontbelievethebyte.state.PLAY";
-    public static final String STATE_PLAYLIST_POSITION = "com.dontbelievethebyte.state.PAUSE";
-    public static final String STATE_CURRENT_SONG_TITLE = "com.dontbelievethebyte.state.SONG_TITLE";
-    public static final String STATE_NO_MEDIA = "com.dontbelievethebyte.state.NO_MEDIA";
 }
