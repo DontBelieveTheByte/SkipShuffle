80363299d1ed0744210839784c1e665413511edc
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-17 05:25:09 -0400

    Added Projections for querying the media store.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/media/Projections.java
@@ -0,0 +1,52 @@
+package com.dontbelievethebyte.skipshuffle.media;
+
+import android.provider.MediaStore;
+
+public class Projections {
+
+    public final static String[] albums = {
+            MediaStore.Audio.Albums._ID,
+            MediaStore.Audio.Albums.ALBUM,
+            MediaStore.Audio.Albums.ALBUM_ART,
+            MediaStore.Audio.Albums.ARTIST,
+            MediaStore.Audio.Albums.NUMBER_OF_SONGS
+    };
+
+    public final static String[] songs = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DURATION,
+    };
+
+    public final static String[] artists = {
+            MediaStore.Audio.Artists._ID,
+            MediaStore.Audio.Artists.ARTIST,
+            MediaStore.Audio.Artists.NUMBER_OF_ALBUMS,
+            MediaStore.Audio.Artists.NUMBER_OF_TRACKS,
+    };
+
+    public final static String[] genres = {
+            MediaStore.Audio.Genres._ID,
+            MediaStore.Audio.Genres.NAME
+    };
+
+    public final static String[] song = {
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.TITLE_KEY,
+            MediaStore.Audio.Media.DATA,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ARTIST_ID,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.TRACK
+    };
+    public final static String[] artist = {
+            MediaStore.Audio.Artists.Albums.ALBUM,
+            MediaStore.Audio.Artists.Albums.ALBUM_KEY,
+    };
+    public final static String[] genre = song;
+    public final static String[] album  = song;
+}
