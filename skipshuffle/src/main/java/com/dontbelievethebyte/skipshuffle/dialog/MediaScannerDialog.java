253a7e7e6299d2b59f97660575cc4d88f140362b
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-19 00:11:41 -0400

    Simplified Media scanner dialog.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/dialog/MediaScannerDialog.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/dialog/MediaScannerDialog.java
@@ -4,9 +4,6 @@ package com.dontbelievethebyte.skipshuffle.dialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
 
 import com.dontbelievethebyte.skipshuffle.R;
 import com.dontbelievethebyte.skipshuffle.activities.BaseActivity;
@@ -31,11 +28,8 @@ public class MediaScannerDialog {
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which)
                     {
-                        baseActivity.sendBroadcast(
-                                new Intent(Intent.ACTION_MEDIA_MOUNTED,
-                                        Uri.parse("file://" + Environment.getExternalStorageDirectory()))
-                        );
                         dialog.dismiss();
+                        baseActivity.handleStartMediaScan();
                     }
                 }
         );
