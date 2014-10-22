0502f461ac54102c2e8058f43509861da8a99bbc
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-22 18:16:51 -0400

    Return media player instance.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/services/MediaPlayerServiceConnection.java
@@ -0,0 +1,32 @@
+package com.dontbelievethebyte.skipshuffle.services;
+
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+import com.dontbelievethebyte.skipshuffle.exceptions.NoMediaPlayerException;
+
+public class MediaPlayerServiceConnection implements ServiceConnection {
+
+    private SkipShuffleMediaPlayer mediaPlayer;
+
+    public SkipShuffleMediaPlayer getMediaPlayer() throws NoMediaPlayerException
+    {
+        if (null == mediaPlayer)
+            throw new NoMediaPlayerException();
+        return mediaPlayer;
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName className, IBinder service)
+    {
+        // We've bound to LocalService, cast the IBinder and get LocalService instance
+        SkipShuffleMediaPlayer.MediaPlayerBinder binder = (SkipShuffleMediaPlayer.MediaPlayerBinder) service;
+        mediaPlayer = binder.getService();
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName arg0) {
+        mediaPlayer = null;
+    }
+}
