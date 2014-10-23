a3f2349517d63687322dba14fa65d1548f5d96be
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-23 03:24:33 -0400

    Commands will use reflection.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/service/SkipShuflleMediaPlayerCommandsContract.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/service/SkipShuflleMediaPlayerCommandsContract.java
@@ -2,9 +2,9 @@ package com.dontbelievethebyte.skipshuffle.service;
 
 public class SkipShuflleMediaPlayerCommandsContract {
     public static final String COMMAND = "com.dontbelievethebyte.cmd.COMMAND";
-    public static final String CMD_PLAY_PAUSE_TOGGLE = "com.dontbelievethebyte.cmd.PLAY";
-    public static final String CMD_SKIP = "com.dontbelievethebyte.cmd.SKIP";
-    public static final String CMD_PREV = "com.dontbelievethebyte.cmd.PREV";
-    public static final String CMD_SHUFFLE_PLAYLIST = "com.dontbelievethebyte.cmd.SHUFFLE_PLAYLIST";
+    public static final String CMD_PLAY_PAUSE_TOGGLE = "doPlay";
+    public static final String CMD_SKIP = "doSkip";
+    public static final String CMD_PREV = "doPrev";
+    public static final String CMD_SHUFFLE_PLAYLIST = "doShuffle";
     public static final String CMD_SET_PLAYLIST_CURSOR_POSITION = "com.dontbelievethebyte.state.PLAYLIST_ID";
 }
