7e257304c7ba7cbaf66d0a60bd6b10a469cc24a5
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-23 03:34:57 -0400

    Changed play with overloading

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/activities/PlaylistActivity.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/activities/PlaylistActivity.java
@@ -8,9 +8,11 @@ import android.widget.TextView;
 
 import com.dontbelievethebyte.skipshuffle.R;
 import com.dontbelievethebyte.skipshuffle.adapters.PlaylistAdapter;
-import com.dontbelievethebyte.skipshuffle.persistance.DbHandler;
+import com.dontbelievethebyte.skipshuffle.exceptions.NoMediaPlayerException;
+import com.dontbelievethebyte.skipshuffle.exceptions.PlaylistEmptyException;
 import com.dontbelievethebyte.skipshuffle.playlists.PlaylistInterface;
-import com.dontbelievethebyte.skipshuffle.ui.DrawableMapper;
+import com.dontbelievethebyte.skipshuffle.service.SkipShuffleMediaPlayer;
+import com.dontbelievethebyte.skipshuffle.ui.mapper.DrawableMapper;
 import com.dontbelievethebyte.skipshuffle.ui.PlaylistUI;
 import com.dontbelievethebyte.skipshuffle.ui.UIFactory;
 
@@ -19,13 +21,18 @@ public class PlaylistActivity extends BaseActivity implements AdapterView.OnItem
 
     private PlaylistAdapter playlistAdapter;
     private PlaylistInterface playlist;
-    private DbHandler dbHandler;
     private ListView listView;
 
     @Override
     protected void handleBackPressed()
     {
 
+    }
+
+    @Override
+    protected int getViewStub()
+    {
+        return 0;
     }
 
     protected void loadPlaylist(PlaylistInterface playlist)
@@ -48,61 +55,93 @@ public class PlaylistActivity extends BaseActivity implements AdapterView.OnItem
     {
         super.onResume();
         preferencesHelper.registerCallBack(this);
-        dbHandler = new DbHandler(getApplicationContext());
-        loadPlaylist(mediaPlayer.getPlaylist());
+//        loadPlaylist(mediaPlayer.getPlaylist());
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
     {
-        if ( (playlist.getPosition() == position) && ((mediaPlayer.getPlayerWrapper().isPlaying())) ) {
-            ImageView imageView = (ImageView) view.findViewById(R.id.track_image);
-            imageView.setImageDrawable(
-                    getResources().getDrawable(
-                            DrawableMapper.getPause(preferencesHelper.getUIType())
-                    )
-            );
-            mediaPlayer.getPlayerWrapper().doPause();
-            ui.doPause();
-        } else {
-            mediaPlayer.getPlayerWrapper().doJump(playlist.getPosition());
-            ui.doPlay();
+        try {
+            SkipShuffleMediaPlayer mediaPlayer = getMediaPlayer();
+            if ( (playlist.getPosition() == position) && ((mediaPlayer.isPlaying())) ) {
+                ImageView imageView = (ImageView) view.findViewById(R.id.track_image);
+                imageView.setImageDrawable(
+                        getResources().getDrawable(
+                                DrawableMapper.getPause(preferencesHelper.getUIType())
+                        )
+                );
+                mediaPlayer.doPause();
+                ui.doPause();
+            } else {
+                mediaPlayer.doPlay(playlist.getPosition());
+                ui.doPlay();
+            }
+        } catch (NoMediaPlayerException noMediaPlayerException) {
+            handleNoMediaPlayerException(noMediaPlayerException);
+        } catch (PlaylistEmptyException playlistEmptyException) {
+            handlePlaylistEmptyException();
         }
+    }
+
+    public void handlePlaylistEmptyException()
+    {
+        preferencesHelper.setLastPlaylist(0);
+        preferencesHelper.setLastPlaylistPosition(0);
     }
 
     @Override
     protected void setUI(Integer type)
     {
-        ui = UIFactory.createPlaylistUI(this, type);
-        ui.playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.getPlayerWrapper().doPlay();
-            }
-        });
+        try {
+            final SkipShuffleMediaPlayer mediaPlayer = getMediaPlayer();
+            ui = UIFactory.createPlaylistUI(this, type);
+            ui.playBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        mediaPlayer.doPlay();
+                    } catch (PlaylistEmptyException playlistEmptyException) {
+                        handlePlaylistEmptyException();
+                    }
+                }
+            });
 
-        ui.prevBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.getPlayerWrapper().doPrev();
-            }
-        });
+            ui.prevBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        mediaPlayer.doPrev();
+                    } catch (PlaylistEmptyException playlistEmptyException) {
+                        handlePlaylistEmptyException();
+                    }
+                }
+            });
 
-        ui.skipBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.getPlayerWrapper().doSkip();
-            }
-        });
+            ui.skipBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        mediaPlayer.doSkip();
+                    } catch (PlaylistEmptyException playlistEmptyException) {
+                        handlePlaylistEmptyException();
+                    }
+                }
+            });
 
-        ui.shuffleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.getPlayerWrapper().doShuffle();
-            }
-        });
+            ui.shuffleBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        mediaPlayer.doShuffle();
+                    } catch (PlaylistEmptyException playlistEmptyException) {
+                        handlePlaylistEmptyException();
+                    }
+                }
+            });
 
-
-        setNavigationDrawerContent();
+            setNavigationDrawerContent();
+        } catch (NoMediaPlayerException noMediaPlayerException) {
+            handleNoMediaPlayerException(noMediaPlayerException);
+        }
     }
 }
--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/service/SkipShuffleMediaPlayer.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/service/SkipShuffleMediaPlayer.java
@@ -1,230 +1,58 @@
 package com.dontbelievethebyte.skipshuffle.service;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
-import com.dontbelievethebyte.skipshuffle.R;
+import com.dontbelievethebyte.skipshuffle.activities.BaseActivity;
+import com.dontbelievethebyte.skipshuffle.callbacks.HeadsetPluggedStateCallback;
+import com.dontbelievethebyte.skipshuffle.callbacks.MediaPlayerCommandsCallback;
 import com.dontbelievethebyte.skipshuffle.callbacks.PlaylistChangedCallback;
+import com.dontbelievethebyte.skipshuffle.callbacks.TrackCompleteCallback;
+import com.dontbelievethebyte.skipshuffle.exceptions.AudioTrackLoadingException;
 import com.dontbelievethebyte.skipshuffle.exceptions.PlaylistEmptyException;
 import com.dontbelievethebyte.skipshuffle.notification.PlayerNotification;
 import com.dontbelievethebyte.skipshuffle.playlists.PlaylistInterface;
 import com.dontbelievethebyte.skipshuffle.playlists.Track;
 import com.dontbelievethebyte.skipshuffle.preferences.PreferencesHelper;
-import com.dontbelievethebyte.skipshuffle.utilities.ToastHelper;
+import com.dontbelievethebyte.skipshuffle.service.broadcastreceiver.CommandsBroadcastReceiver;
+import com.dontbelievethebyte.skipshuffle.service.wrapper.AndroidPlayerWrapper;
 
-import org.json.JSONException;
+public class SkipShuffleMediaPlayer extends Service implements PlaylistChangedCallback,
+                                                               HeadsetPluggedStateCallback,
+                                                               MediaPlayerCommandsCallback,
+                                                               TrackCompleteCallback{
 
-import java.io.IOException;
-
-public class SkipShuffleMediaPlayer extends Service implements PlaylistChangedCallback {
-
-    private ClientCommandsBroadcastReceiver clientCommandsBroadcastReceiver;
+    private CommandsBroadcastReceiver clientCommandsBroadcastReceiver;
     private AndroidPlayerWrapper playerWrapper;
     private PreferencesHelper preferencesHelper;
-    private ToastHelper toastHelper;
     private PlayerNotification notification;
+    private int lastSeekPosition = 0;
+    private Track currentTrack;
 
-    public class AndroidPlayerWrapper implements MediaPlayer.OnPreparedListener,
-                                                 MediaPlayer.OnCompletionListener,
-                                                 MediaPlayer.OnSeekCompleteListener {
-        private int seekPosition = 0;
-        private MediaPlayer mp;
-        private PlaylistInterface playlist;
-
-        private AndroidPlayerWrapper()
-        {
-            toastHelper = new ToastHelper(getApplicationContext());
-            mp = new MediaPlayer();
-            mp.setOnCompletionListener(this);
-            mp.setOnPreparedListener(this);
-            mp.setOnSeekCompleteListener(this);
-        }
-
-        @Override
-        public void onPrepared(MediaPlayer mediaPlayer)
-        {
-            if (seekPosition > 0) {
-                mp.seekTo(seekPosition);
-            } else {
-                mp.start();
-            }
-        }
-
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer)
-        {
-            doSkip();
-        }
-
-        @Override
-        public void onSeekComplete(MediaPlayer mediaPlayer)
-        {
-            mp.start();
-        }
-
-        public void doJump(Integer playlistPosition)
-        {
-            if (null != playlistPosition) {
-                playlist.setPosition(playlistPosition);
-                seekPosition = 0;
-                doPlay();
-            }
-        }
-
-        public void doPlay()
-        {
-            try {
-                if (!mp.isPlaying() && seekPosition > 0) {
-                    loadAudioFile(playlist.getCurrent());
-                    mp.seekTo(seekPosition);
-
-                } else {
-                    loadAudioFile(playlist.getCurrent());
-                }
-            } catch (PlaylistEmptyException e){
-                //Reload the first playlist in case of error.
-                preferencesHelper.setLastPlaylist(1);
-                preferencesHelper.setLastPlaylistPosition(0);
-                toastHelper.showShortToast(
-                        getString(R.string.playlist_error)
-                );
-            }
-        }
-
-        public void doPause()
-        {
-            if (mp.isPlaying()) {
-                mp.pause();
-                seekPosition = mp.getCurrentPosition();
-            }
-        }
-
-        public void doSkip()
-        {
-            if (seekPosition > 0) {
-                seekPosition = 0;
-            }
-            loadAudioFile(playlist.getNext());
-        }
-
-        public void doPrev()
-        {
-            if (seekPosition > 0) {
-                seekPosition = 0;
-            }
-            loadAudioFile(playlist.getPrev());
-        }
-
-        public void doShuffle()
-        {
-            try {
-                playlist.shuffle();
-                playlist.setPosition(0);
-                seekPosition = 0;
-                loadAudioFile(playlist.getFirst());
-            } catch (PlaylistEmptyException e){
-                toastHelper.showShortToast(
-                        getString(R.string.shuffle_empty_playlist)
-                );
-            }
-            doPlay();
-        }
-
-        public boolean isPlaying()
-        {
-            return mp.isPlaying();
-        }
-
-        public void setPlaylist(PlaylistInterface playlist)
-        {
-            this.playlist = playlist;
-        }
-
-        private void loadAudioFile(Track track)
-        {
-            try {
-                mp.reset();
-                mp.setDataSource(track.getPath());
-                mp.prepare();
-            } catch (IOException e) {
-                toastHelper.showShortToast(
-                        getString(R.string.player_wrapper_song_error) + track.getPath()
-                );
-                doSkip();
-            } catch (IllegalArgumentException e) {
-                toastHelper.showShortToast(
-                        getString(R.string.player_wrapper_song_error) + track.getPath()
-                );
-                doSkip();
-            }
+    @Override
+    public void onCommand(String command, Integer newCursorPosition) {
+        java.lang.reflect.Method method;
+        try {
+            method = this.getClass().getMethod(command);
+            if (null != newCursorPosition)
+                method.invoke(this);
+            else
+                method.invoke(this);
+        } catch (Exception e) {
+            handleCommandException(e);
         }
     }
 
-    public AndroidPlayerWrapper getPlayerWrapper()
+    private void handleCommandException(Exception exception)
     {
-        return playerWrapper;
+        Log.d(BaseActivity.TAG, exception.getMessage());
     }
 
-    private class ClientCommandsBroadcastReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent)
-        {
-            if (intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.COMMAND)) {
-                handleCommand(
-                        intent.getStringExtra(SkipShuflleMediaPlayerCommandsContract.COMMAND),
-                        intent
-                );
-            } else if (Intent.ACTION_HEADSET_PLUG.equals(intent.getAction())) {
-                handleHeadsetIntent(intent, isInitialStickyBroadcast());
-            }
-            notification.showNotification();
-            preferencesHelper.setLastPlaylistPosition(0);
-        }
-
-        private void handleCommand(String command, Intent intent)
-        {
-            if (SkipShuflleMediaPlayerCommandsContract.CMD_PLAY_PAUSE_TOGGLE.equals(command)){
-                if (intent.hasExtra(SkipShuflleMediaPlayerCommandsContract.CMD_SET_PLAYLIST_CURSOR_POSITION)) {
-                    playerWrapper.doJump(
-                            intent.getIntExtra(
-                                    SkipShuflleMediaPlayerCommandsContract.CMD_SET_PLAYLIST_CURSOR_POSITION,
-                                    0
-                            )
-                    );
-                } else {
-                    if (playerWrapper.isPlaying()) {
-                        playerWrapper.doPause();
-                    } else {
-                        playerWrapper.doPlay();
-                    }
-                }
-            } else if (SkipShuflleMediaPlayerCommandsContract.CMD_SKIP.equals(command)) {
-                playerWrapper.doSkip();
-            } else if (SkipShuflleMediaPlayerCommandsContract.CMD_PREV.equals(command)) {
-                playerWrapper.doPrev();
-            } else if (SkipShuflleMediaPlayerCommandsContract.CMD_SHUFFLE_PLAYLIST.equals(command)){
-                playerWrapper.doShuffle();
-            }
-        }
-
-        private void handleHeadsetIntent(Intent intent, boolean isInitialStickBroadCast)
-        {
-            boolean isHeadphonesPlugged = (intent.getIntExtra("state", 0) > 0) ;//Transform state to boolean
-            boolean isValidDespiteInitialStickBroadCast = isHeadphonesPlugged && !isInitialStickBroadCast;//Filter out sticky broadcast on service start.
-
-            if (!playerWrapper.isPlaying() && isValidDespiteInitialStickBroadCast) {
-                playerWrapper.doPause();
-            }
-        }
-    }
-
-    public class MediaPlayerBinder extends Binder {
+    public class MediaPlayerBinder extends Binder
+    {
         public SkipShuffleMediaPlayer getService()
         {
             return SkipShuffleMediaPlayer.this;
@@ -240,35 +68,19 @@ public class SkipShuffleMediaPlayer extends Service implements PlaylistChangedCa
     @Override
     public void onCreate()
     {
-        registerMediaPlayerBroadcastReceiver();
-
+        clientCommandsBroadcastReceiver = new CommandsBroadcastReceiver(this);
+        clientCommandsBroadcastReceiver.register();
         preferencesHelper = new PreferencesHelper(getApplicationContext());
-
         notification = new PlayerNotification(this);
-
-//        try {
-////            playlist = new RandomPlaylist(
-////                    preferencesHelper.getLastPlaylist(),
-////                    dbHandler
-////            );
-//
-//            playlist.setPosition(preferencesHelper.getLastPlaylistPosition());
-//
-//            playerWrapper = new AndroidPlayerWrapper();
-//
-//            playerWrapper.setPlaylist(playlist);
-//
-//        } catch (JSONException jsonException) {
-//            handleJSONException(jsonException);
-//        }
+        playerWrapper = new AndroidPlayerWrapper(this);
     }
 
     @Override
     public void onDestroy()
     {
-        unregisterMediaPlayerBroadcastReceiver();
+        clientCommandsBroadcastReceiver.unregister();
         notification.cancel();
-        playerWrapper.doPause();
+        doPause();
         preferencesHelper.setLastPlaylist(0);
         preferencesHelper.setLastPlaylistPosition(0);
     }
@@ -281,50 +93,85 @@ public class SkipShuffleMediaPlayer extends Service implements PlaylistChangedCa
     }
 
     @Override
+    public void onHeadsetStateChanged(boolean isHeadsetPluggedIn) {
+        if (!playerWrapper.isPlaying() && isHeadsetPluggedIn)
+            doPause();
+        notification.showNotification();
+    }
+
+    @Override
     public void onPlaylistChange(long playlistId)
     {
-//        try {
-//            playlist = new RandomPlaylist(
-//                    playlistId,
-//                    dbHandler
-//            );
-//            playlist.setPosition(0);
-//            playerWrapper.setPlaylist(playlist);
-//        } catch (JSONException jsonException){
-//            handleJSONException(jsonException);
-//        }
     }
 
-    private void handleJSONException(JSONException jSONException)
+    @Override
+    public void onTrackComplete()
     {
-        toastHelper.showLongToast(
-                String.format(
-                        getString(R.string.playlist_load_error),
-                        preferencesHelper.getLastPlaylist()
-                )
-        );
-        preferencesHelper.setLastPlaylist(1);
-        preferencesHelper.setLastPlaylistPosition(0);
-    }
-
-    public void registerMediaPlayerBroadcastReceiver()
-    {
-        if (null == clientCommandsBroadcastReceiver) {
-            clientCommandsBroadcastReceiver = new ClientCommandsBroadcastReceiver();
+        try {
+            doSkip();
+        } catch (PlaylistEmptyException playlistEmptyException) {
+            handlePlaylistEmptyException();
         }
-
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(SkipShuflleMediaPlayerCommandsContract.COMMAND);
-        intentFilter.addAction(Intent.ACTION_HEADSET_PLUG);
-        intentFilter.addAction(Intent.ACTION_MEDIA_BUTTON);
-        registerReceiver(clientCommandsBroadcastReceiver, intentFilter);
     }
 
-    public void unregisterMediaPlayerBroadcastReceiver()
+    public void doPlay() throws PlaylistEmptyException
     {
-        if (clientCommandsBroadcastReceiver != null) {
-            unregisterReceiver(clientCommandsBroadcastReceiver);
+        if (playerWrapper.isPlaying())
+            doPause();
+        else {
+            try {
+                resetSeekPosition();
+                playerWrapper.loadAudioFile(currentTrack);
+            } catch (AudioTrackLoadingException audioLoadingTrackException) {
+                handleAudioLoadingTrackException(audioLoadingTrackException);
+            }
         }
+    }
+
+    public void doPlay(int playlistPosition) throws PlaylistEmptyException
+    {
+        resetSeekPosition();
+        doPlay();
+    }
+
+    private void handleAudioLoadingTrackException(AudioTrackLoadingException audioTrackLoadingException) throws PlaylistEmptyException
+    {
+        doSkip();
+    }
+
+    public void doPause()
+    {
+        lastSeekPosition = (playerWrapper.isPlaying()) ?
+                playerWrapper.pausePlayingTrack() :
+                0;
+    }
+
+    public void doSkip() throws PlaylistEmptyException
+    {
+        resetSeekPosition();
+        doPlay();
+    }
+
+    public void doPrev() throws PlaylistEmptyException
+    {
+        resetSeekPosition();
+        doPlay();
+    }
+
+    public void doShuffle() throws PlaylistEmptyException
+    {
+        resetSeekPosition();
+        doPlay();
+    }
+
+    public boolean isPlaying()
+    {
+        return playerWrapper.isPlaying();
+    }
+
+    public int getLastSeekPosition()
+    {
+        return lastSeekPosition;
     }
 
     public PreferencesHelper getPreferencesHelper()
@@ -332,8 +179,19 @@ public class SkipShuffleMediaPlayer extends Service implements PlaylistChangedCa
         return preferencesHelper;
     }
 
-    public PlaylistInterface getPlaylist(){
+    public PlaylistInterface getPlaylist()
+    {
         return null;
     }
 
+    public void handlePlaylistEmptyException()
+    {
+        preferencesHelper.setLastPlaylist(0);
+        preferencesHelper.setLastPlaylistPosition(0);
+    }
+
+    private void resetSeekPosition()
+    {
+        lastSeekPosition = 0;
+    }
 }
