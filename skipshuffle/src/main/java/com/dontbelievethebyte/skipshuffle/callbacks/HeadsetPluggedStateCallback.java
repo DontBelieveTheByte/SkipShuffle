caa6e23f5d978c08d437dd2f7273e62db4e1e09e
killthesystem <ever.livin.never.fearful@gmail.com>
2014-10-23 03:24:12 -0400

    Added new callback interfaces for headset and media player commands.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/callbacks/HeadsetPluggedStateCallback.java
@@ -0,0 +1,5 @@
+package com.dontbelievethebyte.skipshuffle.callbacks;
+
+public interface HeadsetPluggedStateCallback {
+    public void onHeadsetStateChanged(boolean isHeadsetPluggedIn);
+}
---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/callbacks/MediaPlayerCommandsCallback.java
@@ -0,0 +1,5 @@
+package com.dontbelievethebyte.skipshuffle.callbacks;
+
+public interface MediaPlayerCommandsCallback {
+    public void onCommand(String command, Integer newCursorPosition);
+}
