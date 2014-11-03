91bb49758a69330b05a0208e524c66bfe972dbf9
killthesystem <ever.livin.never.fearful@gmail.com>
2014-11-03 03:11:03 -0500

    Custom typeface class.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/CustomTypeface.java
@@ -0,0 +1,30 @@
+package com.dontbelievethebyte.skipshuffle.ui;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+
+import com.dontbelievethebyte.skipshuffle.ui.mapper.TypeFaceMapper;
+
+public class CustomTypeface {
+
+    private int uiType;
+    private Typeface typeface;
+    private Activity activity;
+
+    public CustomTypeface(Activity activity, int uiType)
+    {
+        this.activity = activity;
+        this.uiType = uiType;
+    }
+
+    public Typeface getTypeFace()
+    {
+        if (null == typeface) {
+            typeface = Typeface.createFromAsset(
+                    activity.getAssets(),
+                    TypeFaceMapper.getTypeFace(uiType)
+            );
+        }
+        return typeface;
+    }
+}
