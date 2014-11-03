9a2e62dae7f546fa2c1d7e4aa61bb0cb01e2c335
killthesystem <ever.livin.never.fearful@gmail.com>
2014-11-03 03:29:57 -0500

    Wrapper class for layout id.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/ContentArea.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/ContentArea.java
@@ -1,10 +1,16 @@
 package com.dontbelievethebyte.skipshuffle.ui;
 
 public class ContentArea {
+
     private int layoutId;
 
     public ContentArea(int layoutId)
     {
         this.layoutId = layoutId;
+    }
+
+    public int getLayoutId()
+    {
+        return layoutId;
     }
 }
