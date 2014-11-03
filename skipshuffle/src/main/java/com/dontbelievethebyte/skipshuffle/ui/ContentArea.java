e8eaa4d5316b9bff4b1255102d831cffaf134845
killthesystem <ever.livin.never.fearful@gmail.com>
2014-11-03 04:38:12 -0500

    Init on constructor.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/ContentArea.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/ContentArea.java
@@ -1,12 +1,15 @@
 package com.dontbelievethebyte.skipshuffle.ui;
 
+import android.app.Activity;
+
 public class ContentArea {
 
     private int layoutId;
 
-    public ContentArea(int layoutId)
+    public ContentArea(Activity activity, int layoutId)
     {
         this.layoutId = layoutId;
+        activity.setContentView(layoutId);
     }
 
     public int getLayoutId()
