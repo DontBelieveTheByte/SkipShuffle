12c01d356b2c4d05922647d8c0efcbc59f5d2096
killthesystem <ever.livin.never.fearful@gmail.com>
2014-11-04 04:44:10 -0500

    Formatting.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/elements/ContentArea.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/elements/ContentArea.java
@@ -13,6 +13,7 @@ public class ContentArea implements UIElementCompositeInterface {
         activity.setContentView(layoutId);
         bottomLayout = (ViewGroup) activity.findViewById(R.id.bottom);
     }
+
     public ViewGroup getBottomLayout()
     {
         return bottomLayout;
