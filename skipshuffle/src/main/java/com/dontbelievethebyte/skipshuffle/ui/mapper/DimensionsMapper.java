387771ca755f1c552ad98f8701f39aeadce5d978
J-F B <dontbelievethebyte@gmail.com>
2014-12-11 17:14:38 -0500

    Removed unused code.

--- skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/mapper/DimensionsMapper.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/mapper/DimensionsMapper.java
@@ -21,63 +21,18 @@ public final class DimensionsMapper {
 
     public final static class Player {
 
-        public final static class Padding {
-            public final static class Landscape {
-                public final static double top = .25;
-                public final static double right = .15;
-                public final static double left = .15;
-            }
-            public final static class Portrait {
-                public final static double top = .1;
-                public final static double right = .05;
-                public final static double left = .05;
-            }
-        }
-
-        //Top buttons
-        public final static class Top {
-            public final static class Landscape {
-                public final static double buttonSize = .2;
-            }
-            public final static class Portrait {
-                public final static double buttonSize = .4;
-            }
-        }
-
         //Bottom center buttons
         public final static class Center {
             public final static class Landscape {
                 public final static double width = .7;
                 public final static double textSize = .035;
-                public final static double textHeight = 1.8;
 
             }
             public final static class Portrait {
                 public final static double width = .8;
                 public final static double textSize = .05;
-                public final static double textHeight = 4;
 
             }
-        }
-
-        //Bottom buttons
-        public final static class Bottom {
-            public final static class Landscape {
-                public final static double buttonSideSize = Top.Landscape.buttonSize * .9;
-                public final static double buttonCenterSize = Top.Landscape.buttonSize * .8;
-            }
-            public final static class Portrait {
-                public final static double buttonSideSize = Top.Portrait.buttonSize * .65;
-                public final static double buttonCenterSize = Top.Portrait.buttonSize * .4;
-            }
-        }
-
-        public final static class SongLabel {
-            public final static class Landscape {
-                public final static int textSize = 34;
-            }
-            public final static class Portrait {
-                public final static int textSize = 44;            }
         }
     }
 }
