d9aff95a95c762210f0e839c9a95f4adfa946bdb
J-F B <dontbelievethebyte@gmail.com>
2015-01-05 21:11:06 -0500

    Created contract.

---
+++ skipshuffle/src/main/java/com/dontbelievethebyte/skipshuffle/ui/remote/remote/widget/WidgetContract.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2015. Jean-Francois Berube, all rights reserved.
+ */
+
+package com.dontbelievethebyte.skipshuffle.ui.remote.remote.widget;
+
+public class WidgetContract {
+    public static final String TITLE= "title";
+    public static final String ARTIST = "artist";
+    public static final String IS_PLAYING = "isPlaying";
+    public static final String THEME = "theme";
+}
