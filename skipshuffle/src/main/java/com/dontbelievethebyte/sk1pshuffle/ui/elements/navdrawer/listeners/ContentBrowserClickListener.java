f2aab68fa6c0d2cc2c7588e1a29c6bc34f66633a
J-F B <dontbelievethebyte@gmail.com>
2015-01-15 15:40:03 -0500

    Added content browser activity.

--- skipshuffle/src/main/java/com/dontbelievethebyte/sk1pshuffle/ui/elements/navdrawer/listeners/ContentBrowserClickListener.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/sk1pshuffle/ui/elements/navdrawer/listeners/ContentBrowserClickListener.java
@@ -5,6 +5,7 @@
 package com.dontbelievethebyte.sk1pshuffle.ui.elements.navdrawer.listeners;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Gravity;
@@ -13,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.dontbelievethebyte.sk1pshuffle.activities.BaseActivity;
+import com.dontbelievethebyte.sk1pshuffle.activities.ContentBrowserActivity;
 
 public class ContentBrowserClickListener implements ListView.OnItemClickListener {
 
@@ -47,20 +49,28 @@ public class ContentBrowserClickListener implements ListView.OnItemClickListener
     private void browseSongs(Context context)
     {
         Log.d(BaseActivity.TAG , "$$$ BROWSING SONGS");
+        Intent intent = new Intent(context, ContentBrowserActivity.class);
+        context.startActivity(intent);
     }
 
     private void browseArtists(Context context)
     {
         Log.d(BaseActivity.TAG , "$$$ BROWSING ARTISTS");
+        Intent intent = new Intent(context, ContentBrowserActivity.class);
+        context.startActivity(intent);
     }
 
     private void browseAlbums(Context context)
     {
         Log.d(BaseActivity.TAG , "$$$ BROWSING ALBUMS");
+        Intent intent = new Intent(context, ContentBrowserActivity.class);
+        context.startActivity(intent);
     }
 
     private void browseGenres(Context context)
     {
         Log.d(BaseActivity.TAG , "$$$ BROWSING GENRES");
+        Intent intent = new Intent(context, ContentBrowserActivity.class);
+        context.startActivity(intent);
     }
 }
