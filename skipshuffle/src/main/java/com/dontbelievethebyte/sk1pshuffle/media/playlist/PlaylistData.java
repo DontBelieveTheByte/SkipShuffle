e1a885f729ecf68530873ed8a0c5ad530fae3f22
J-F B <dontbelievethebyte@gmail.com>
2015-01-22 22:36:22 -0500

    Removed parcelable.

--- skipshuffle/src/main/java/com/dontbelievethebyte/sk1pshuffle/media/playlist/PlaylistData.java
+++ skipshuffle/src/main/java/com/dontbelievethebyte/sk1pshuffle/media/playlist/PlaylistData.java
@@ -4,54 +4,11 @@
 
 package com.dontbelievethebyte.sk1pshuffle.media.playlist;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class PlaylistData implements Parcelable{
-
-    public static final Parcelable.Creator<PlaylistData> CREATOR = new Parcelable.Creator<PlaylistData>()
-    {
-        @Override
-        public PlaylistData createFromParcel(Parcel incoming)
-        {
-            PlaylistData playlistData = new PlaylistData();
-            playlistData.currentPosition = incoming.readInt();
-            playlistData.isShuffleOn = (incoming.readByte() != 0);
-            playlistData.trackIds = new ArrayList<>();
-            incoming.readList(playlistData.trackIds, getClass().getClassLoader());
-            playlistData.shuffledTrackIds = new ArrayList<>();
-            incoming.readList(playlistData.shuffledTrackIds, getClass().getClassLoader());
-            return playlistData;
-        }
-
-        @Override
-        public PlaylistData[] newArray(int size)
-        {
-            return new PlaylistData[size];
-        }
-    };
-
+public class PlaylistData {
     public int currentPosition = 0;
     public boolean isShuffleOn = false;
     public List<String> trackIds;
     public List<String> shuffledTrackIds;
-
-    @Override
-    public int describeContents()
-    {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel destination, int flags)
-    {
-        destination.writeInt(currentPosition);
-        destination.writeByte((byte) (isShuffleOn ? 1 : 0));
-        destination.writeList(trackIds);
-        destination.writeList(shuffledTrackIds);
-    }
-
 }
